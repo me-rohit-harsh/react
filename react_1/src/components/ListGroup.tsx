fc90926056626c36b513e2fee51588c883c6534e
Rohit kumar <rohitkumarah369@gmail.com>
2024-12-20 23:58:41 +0530

    if condition

--- react_1/src/components/ListGroup.tsx
+++ react_1/src/components/ListGroup.tsx
@@ -1,10 +1,16 @@
 import "../App.css";
 
 function ListGroup() {
-    const citys = ["Patna","Delhi","Saran","Gopal"];
+    let citys = ["Patna", "Delhi", "Saran", "Gopal"];
+    citys = [];
+    if (citys.length == 0) {
+        return <> <h1>List Groups</h1>
+            <ul className="list-group">
+                <li className="list-group-item">No any item</li>
+            </ul> </>
+    }
     return (
         <>
-            <h1>List Groups</h1>
             <ul className="list-group">
                 {citys.map((city, index) => (
                     <li key={index} className="list-group-item">{city}</li>
